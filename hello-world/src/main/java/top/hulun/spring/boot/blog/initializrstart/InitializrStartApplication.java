69a410d5b2917381ba91b1ba13511c6fbf37d6a1
hulunbao <hulunbao@gmail.com>
2018-12-16 22:27:15 +0800

    2018-12-16

---
+++ hello-world/src/main/java/top/hulun/spring/boot/blog/initializrstart/InitializrStartApplication.java
@@ -0,0 +1,14 @@
+package top.hulun.spring.boot.blog.initializrstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializrStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitializrStartApplication.class, args);
+	}
+
+}
+
